2dac6a56749640642c0e79f000f85244cf666ca3
GodwinEgo <godwinehile04@gmail.com>
2023-02-02 13:11:12 +0100

    A paint company website

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+/*==================PRELOADER STARTS================ */
+
+$( window ).on( 'load', function ()
+{
+  $( 'body' ).addClass( 'loaded' );
+  openPage( 1 );
+} );
+
+/*==================PRELOADER ENDS================ */
